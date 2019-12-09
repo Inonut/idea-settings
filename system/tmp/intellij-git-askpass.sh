9c44c2745fbc4395a7fc54994d2faf8b231a5177
Vagrant Dev <you@example.com>
2019-12-09 21:12:12 +0000

    update

--- system/tmp/intellij-git-askpass.sh
+++ system/tmp/intellij-git-askpass.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-"/usr/lib/jvm/java-11-openjdk/bin/java" -cp "/home/vagrant/idea-IU/plugins/git4idea/lib/git4idea-rt.jar:/home/vagrant/idea-IU/lib/xmlrpc-2.0.1.jar:/home/vagrant/idea-IU/lib/commons-codec-1.13.jar:/home/vagrant/idea-IU/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp "$@"
+"/usr/lib/jvm/java-13-openjdk/bin/java" -cp "/home/vagrant/idea-IU/plugins/git4idea/lib/git4idea-rt.jar:/home/vagrant/idea-IU/lib/xmlrpc-2.0.1.jar:/home/vagrant/idea-IU/lib/commons-codec-1.13.jar:/home/vagrant/idea-IU/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp "$@"
